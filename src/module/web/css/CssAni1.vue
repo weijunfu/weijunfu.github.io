c16c6d3379e6f9d74700840fc2b9f9e640d7cd8d
ijunfu <ijunfu@163.com>
2025-12-11 22:32:07 +0800

    add css 3d 螺旋动画

---
+++ src/module/web/css/CssAni1.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="box">
+  <div v-for="item in totalNum" :key="item" class="ele" :style="{'--i': item}"></div>
+</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const totalNum = 20
+
+</script>
+<style scoped lang="scss">
+@keyframes run {
+  to {
+    transform: none;
+  }
+}
+.box {
+  width: 360px;
+  height: 70px;
+  perspective: 400px;           // 设置3d
+  transform-style: preserve-3d; // 开启3d
+
+  .ele {
+    position: relative;
+    width: 1px;
+    height: 70px;
+    border-left: 1px #E0FFFF dashed;
+    float: left;
+    margin: 0 8px;
+    transform: rotateX(-360deg);
+    animation: run 2s linear infinite;
+    animation-delay: calc(0.15s * var(--i));
+
+    &::before,
+    &::after { 
+      content: '';
+      position: absolute;
+      width: 10px;
+      height: 10px;
+      background-color: #87CEEB;
+      border-radius: 50%;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+
+    &::before {
+      top: -2px;
+      background-color: #FFD8A0;
+    }
+
+    &::after {
+      bottom: -2px;
+    }
+  
+  }
+}
+</style>
--- src/views/web/index.vue
+++ src/views/web/index.vue
@@ -1,8 +1,13 @@
 <template>
-<h1>Web</h1>
+<div class="container flex flex-column w-full h-full">
+  <Html5 />
+  <CssAni1 />
+</div>
 </template>
 <script setup lang="ts">
+import Html5 from '@/module/web/html/Readme.md';
 
+import CssAni1 from '@/module/web/css/CssAni1.vue'
 </script>
 <style scoped lang="scss">
 
